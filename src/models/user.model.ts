c4622572e3e7f9c0da8e9fce1b18e415811bdcdf
Anthony Ducci <aducci@deloitte.com.au>
2021-06-01 16:31:53 +1000

    initial commit

---
+++ src/models/user.model.ts
@@ -0,0 +1,61 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  dateadded: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  state: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'string',
+  })
+  mobile?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
